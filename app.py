0dbd188701fa02f577b64ed96c85a03e1cccb724
Mario Saravia <149202548+marioisl@users.noreply.github.com>
2025-02-14 14:41:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from api.instrument_controller import instrument_bp
-from api.activity_controller import activity_bp  # Asegúrate de tener activity_controller si lo necesitas
+from api.activity_controller import activity_bp  
 
 app = Flask(__name__)
 
 # Registrar blueprints
 app.register_blueprint(instrument_bp, url_prefix='/api')
-app.register_blueprint(activity_bp, url_prefix='/api')  # Si tienes activity_controller.py
+app.register_blueprint(activity_bp, url_prefix='/api')  #
 
 if __name__ == "__main__":
     app.run(debug=True)
